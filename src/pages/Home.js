956f853c7fea307d290104903c9ca080ecbd0fd3
jen_807 <80chill@naver.com>
2025-01-16 22:14:07 +0900

    ready for pull

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,38 @@
 import styled from "styled-components";
 import homeBg from "./backgroundImg/homeBg.jpg";
+import logo from "../components/img/Logo.svg";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background: url(${homeBg});
+  background: url(${homeBg}) no-repeat center center;
+  background-size: cover;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  padding: 0 0 115px 0;
+  img {
+    width: 1000px;
+    height: auto;
+    margin: 0 auto;
+  }
+
+  h3 {
+    font-size: 40px;
+    font-weight: bold;
+    color: white;
+  }
 `;
-const Logo = styled.div``;
 
 const Home = () => {
   return (
     <Container>
-      <Logo></Logo>
+      <img src={logo} alt="로고" />
+      <Link>
+        <h3>화면을 터치해 시작해보세요</h3>
+      </Link>
     </Container>
   );
 };
