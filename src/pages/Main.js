58826bf66c95c0cf0677728be1bd9f0a46e54d71
preistone <aion97@naver.com>
2025-01-16 21:49:56 +0900

    main bg setting

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import mainBg from "./backgroundImg/mainBg.jpg";
+import mainBox from "./backgroundImg/mainBox.png";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: url(${mainBg});
+
+  img {
+    position: absolute;
+    bottom: -140px;
+    left: 0;
+  }
+
+  @media screen and (max-width: 1000px) {
+    img {
+      bottom: 0;
+    }
+  }
+`;
 const Main = () => {
-  return <></>;
+  return (
+    <Container>
+      <img src={mainBox} alt="박스배경" />
+    </Container>
+  );
 };
 
 export default Main;
