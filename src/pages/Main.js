8183fec1cea20ff36e839b880e1b05faac078ded
preistone <aion97@naver.com>
2025-01-18 18:24:04 +0900

    main style setting

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,7 +8,15 @@ import npc1body from "../components/img/npc/1_body.png";
 import npc1head from "../components/img/npc/1_head.png";
 import chat1_1 from "../components/img/chat/chat1_1.png";
 import chat3 from "../components/img/chat/chat3.png";
+import closeBtn from "../components/img/downarrow.svg";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const HomeBtn = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: lightgreen;
+`;
 
 const Container = styled.div`
   width: 100%;
@@ -67,6 +75,10 @@ const PaintingWrap = styled.div`
   top: 30%;
   left: 50%;
   transform: translate(-50%, -30%);
+
+  :nth-child(2) {
+    background-color: green;
+  }
 
   @media screen and (max-width: 1000px) {
     width: 80%;
@@ -260,7 +272,8 @@ const SubChat = styled.div`
 
 const SubText = styled.div`
   display: flex;
-  width: 240px;
+  /* width: 240px; */
+  width: 100%;
   height: 30px;
   justify-content: space-between;
   align-items: center;
@@ -309,26 +322,111 @@ const SubText = styled.div`
   }
 `;
 
-const Blur = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
+const ModalBackground = styled.div`
+  position: fixed;
   top: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.7);
-  z-index: 100;
-  filter: blur(1px);
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
+const ModalContainer = styled.div`
+  position: relative;
+  width: 500px;
+
+  @media screen and (max-width: 440px) {
+    width: 300px;
+  }
+`;
+
+const EnlargedPainting = styled.div`
+  width: 500px;
+  height: 500px;
+
+  text-align: center;
+  background-color: salmon;
+
+  img {
+    width: 500px;
+    position: absolute;
+    top: -80px;
+    left: 0;
+  }
+
+  @media screen and (max-width: 440px) {
+    height: 300px;
+    width: 300px;
+
+    img {
+      width: 300px;
+      top: -48px;
+    }
+  }
+`;
+
+const CloseButton = styled.button`
+  all: unset;
+  position: fixed;
+  bottom: 8%;
+  left: 50%;
+  transform: translateX(-50%);
+  cursor: pointer;
+
+  @media screen and (max-width: 1400px) {
+    bottom: 5%;
+  }
+
+  @media screen and (max-width: 440px) {
+    bottom: 20%;
+  }
+`;
+
+const paintings = [
+  {
+    id: "left",
+    background: "salmon", // 또는 backgroundImage: "url('/images/left.png')"
+  },
+  {
+    id: "right",
+    background: "green", // 또는 backgroundImage: "url('/images/right.png')"
+  },
+];
+
 const Main = () => {
-  const [isSelected, setIsSelected] = useState(false);
+  const [conversationStage, setConversationStage] = useState(0); //대화관련 상태 입니다.
+  const [selectedPainting, setSelectedPainting] = useState(null); //모달(확대) 상태 입니다.
 
-  const handleSelect = () => {
-    setIsSelected(true);
-  };
-
-  const handleBack = () => {
-    setIsSelected(false);
+  const getChatText = () => {
+    switch (conversationStage) {
+      case 0:
+        return (
+          <>
+            히야~ 정말 멋진 그림들입니다요! <br />
+            자세히 살펴 보시죠~ 헷헷헷
+          </>
+        );
+      case 1:
+        return (
+          <>
+            오~ 좀 더 자세히 보시겠습니까? <br />
+            어떤 그림이 궁금하시죠?
+          </>
+        );
+      case 2:
+        return (
+          <>
+            오! 손님, 결정하셨습니까? <br />
+            어떤 작품을 구매하시겠습니까요?
+          </>
+        );
+      default:
+        return null;
+    }
   };
 
   return (
@@ -336,66 +434,107 @@ const Main = () => {
       <img src={mainBox} alt="박스배경" id="boxBg" />
       {/* <Blur></Blur> */}
 
+      <Link to={"/#"}>
+        <HomeBtn>홈 가기</HomeBtn>
+      </Link>
+      <Link to={"/ending"}>
+        <HomeBtn>엔딩 가기</HomeBtn>
+      </Link>
+
       <Npc>
         <img src={npc1body} alt="여욱이" id="npc-body" />
       </Npc>
 
       <PaintingWrap>
-        <Painting>
+        <Painting style={{ background: paintings[0].background }}>
           <img src={paintframe} alt="액자사진" id="frame" />
         </Painting>
-        <Painting>
+        <Painting style={{ background: paintings[1].background }}>
           <img src={paintframe} alt="액자사진" id="frame" />
         </Painting>
       </PaintingWrap>
 
       <Chat>
         <img src={chat1_1} alt="말풍선이미지" />
-        {!isSelected ? (
-          <h2>
-            히야~ 정말 멋진 그림들입니다요! <br />
-            자세히 살펴 보시죠~ 헷헷헷
-          </h2>
-        ) : (
-          <h2>
-            오! 손님, 결정하셨습니까? <br />
-            어떤 작품을 구매하시겠습니까요?
-          </h2>
-        )}
+        <h2>{getChatText()}</h2>
+
         <SubChat>
           <img src={chat3} alt="선택말풍선이미지" />
           <img src={npc1head} alt="여욱이" id="npc-head" />
-          {/* isSelected 상태에 따라 SubText 내부 문구도 조건부 렌더링 */}
-          {!isSelected ? (
+
+          {/* 단계별 버튼 표시 */}
+          {conversationStage === 0 && (
             <>
-              <SubText>
+              <SubText onClick={() => setConversationStage(1)}>
                 <img src={cursor} alt="손가락" />
                 <p>자세히 볼래!</p>
               </SubText>
-              {/* 이곳에 클릭 이벤트를 연결해서 상태 변경 */}
-              <SubText onClick={handleSelect}>
-                <img src={cursor} alt="손가락" id="secondCusor" />
+              <SubText onClick={() => setConversationStage(2)}>
+                <img src={cursor} alt="손가락" />
                 <h3>선택할래!</h3>
               </SubText>
             </>
-          ) : (
+          )}
+
+          {conversationStage === 1 && (
+            <>
+              <SubText onClick={() => setSelectedPainting("left")}>
+                <img src={cursor} alt="손가락" />
+                <p>왼쪽 그림!</p>
+              </SubText>
+              <SubText onClick={() => setSelectedPainting("right")}>
+                <img src={cursor} alt="손가락" />
+                <h3>오른쪽 그림!</h3>
+              </SubText>
+              <SubText onClick={() => setConversationStage(0)}>
+                <img src={cursor} alt="손가락" />
+                <h4>안궁금해!</h4>
+              </SubText>
+            </>
+          )}
+
+          {conversationStage === 2 && (
             <>
               <SubText>
                 <img src={cursor} alt="손가락" />
                 <p>왼쪽 그림!</p>
               </SubText>
               <SubText>
-                <img src={cursor} alt="손가락" id="secondCusor" />
+                <img src={cursor} alt="손가락" />
                 <h3>오른쪽 그림!</h3>
               </SubText>
-              <SubText onClick={handleBack}>
-                <img src={cursor} alt="손가락" id="thirdCusor" />
+              <SubText onClick={() => setConversationStage(0)}>
+                <img src={cursor} alt="손가락" />
                 <h4>다시 볼래!</h4>
               </SubText>
             </>
           )}
         </SubChat>
       </Chat>
+
+      {selectedPainting && (
+        <ModalBackground onClick={() => setSelectedPainting(null)}>
+          <ModalContainer onClick={(e) => e.stopPropagation()}>
+            <CloseButton onClick={() => setSelectedPainting(null)}>
+              <img src={closeBtn} alt="팝업창닫기" />
+            </CloseButton>
+            <EnlargedPainting
+              style={{
+                background:
+                  selectedPainting === "left"
+                    ? paintings[0].background
+                    : paintings[1].background,
+              }}
+            >
+              {selectedPainting === "left" ? (
+                <img src={paintframe} alt="왼쪽그림확대" />
+              ) : (
+                <img src={paintframe} alt="오른쪽그림확대" />
+              )}
+            </EnlargedPainting>
+          </ModalContainer>
+        </ModalBackground>
+      )}
     </Container>
   );
 };
