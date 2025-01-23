7022d3814e500a8c5795aa5b7ba0a8c3b7d363d5
elena7993 <elenalee7993@gmail.com>
2025-01-23 13:56:51 +0900

    404_edit_2

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,20 +1,27 @@
-import emotion_3 from "../components/img/emotion/emotion_3.png";
+import emotion_4 from "../components/img/emotion/emotion_4.png";
 import chat1_1 from "../components/img/chat/chat1_1.png";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  min-height: 100vh;
   background-color: #000;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
-  padding: 40px 0;
+  padding: 30px 0;
 `;
 const Npc = styled.div`
   width: 250px;
   height: auto;
+  img {
+    @media screen and (max-width: 440px) {
+      width: 190px;
+      transform: translateX(20px);
+    }
+  }
 `;
 const Chat = styled.div`
   width: 600px;
@@ -22,12 +29,12 @@ const Chat = styled.div`
   h2 {
     position: absolute;
     top: 30%;
-    left: 25%;
+    left: 22%;
     color: #827459;
-    font-size: 23px;
+    font-size: 26px;
     font-family: "Noto Sans KR";
     font-weight: 600;
-    line-height: 38px;
+    line-height: 40px;
     span {
       color: #0fb3cf;
     }
@@ -47,14 +54,14 @@ const PageNotFound = () => {
   return (
     <Container>
       <Npc>
-        <img src={emotion_3} alt="여욱이파칭" />
+        <img src={emotion_4} alt="여욱이파칭" />
       </Npc>
 
       <Chat>
         <img src={chat1_1} alt="말풍선1" />
         <h2>
-          이런, 손님... <br />
-          <span>경로</span>를 잘못 찾아오셨습니다요.
+          아이고, 손님... <br />
+          <span>경로</span>를 잘못 찾아오셨습니다요!
         </h2>
       </Chat>
     </Container>
